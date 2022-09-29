dad8ba38b1bf128a6da4130c524cd6f10ea4a1ff
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-09-29 16:17:31 -0400

    Started day 26, Nested loops

---
+++ src/day26_nested_loop/NestedLoopExample.java
@@ -0,0 +1,40 @@
+package day26_nested_loop;
+
+public class NestedLoopExample {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello my first job!");
+
+        }
+        System.out.println("**********************");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello my working space! ");
+
+        }
+        System.out.println("**********************");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello my new working lfe!");
+
+        }
+
+        System.out.println("---------------------------------------------------------------");
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("I will work soon!");
+
+            for (int j = 0; j < 5; j++) {
+                System.out.println(" In sha Allah");
+
+            }
+
+        }
+
+
+
+
+
+    }
+}
