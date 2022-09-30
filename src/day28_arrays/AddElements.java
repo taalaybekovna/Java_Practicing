0ce9ae3f50a5b9a31f33ad4de5af82f327bcc971
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-09-30 17:58:55 -0400

    AddElements

---
+++ src/day28_arrays/AddElements.java
@@ -0,0 +1,21 @@
+package day28_arrays;
+
+import java.util.Arrays;
+
+public class AddElements {
+    /*
+
+        declare and assign an array of numbers
+
+            {3, 6, 10}
+
+        sum all the numbers in the array
+
+     */
+    public static void main(String[] args) {
+
+
+       int[] arr = {3, 6, 10};
+        System.out.println("Hard coded sum: " + (arr[0] + arr[1] + arr[2]));
+    }
+}
