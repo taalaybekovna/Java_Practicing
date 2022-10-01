9a59d80df59107fe3485efa149fed5e9b3815500
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-10-01 19:57:26 -0400

    Started day30. Class CharArray

---
+++ src/day30_arrays/CharArray.java
@@ -0,0 +1,20 @@
+package day30_arrays;
+
+public class CharArray {
+    public static void main(String[] args) {
+
+        char [] letters = {'B', 'a', 'g', 'd', 'a', 't'};
+        System.out.println(letters);
+
+        String name = "Eliza";
+        System.out.println(name);
+
+        System.out.println("--------------------------");
+
+        String name2 = new String(letters);
+        System.out.println(name2);
+
+        char [] letters2 = name.toCharArray();
+        System.out.println(letters2);
+    }
+}
