86598f4af70932cb235c57bad0d7d2978cc87e47
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-10-01 13:09:31 -0400

    ForEachLoopExample

---
+++ src/day28_arrays/ForEchLoopExample.java
@@ -0,0 +1,52 @@
+package day28_arrays;
+
+public class ForEchLoopExample {
+    public static void main(String[] args) {
+
+        int[] nums = {30, 12, 159, 12};
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("traditional " + nums[i]);
+        }
+
+        System.out.println("-------------------------------------------");
+
+        for (int element : nums) {
+            System.out.println("for each: " + element);
+        }
+
+        System.out.println("---------------------------------------------");
+
+        // String example
+
+        String[] classes = {"Java", "Soft skills", "Selenium", "SQL", "API"};
+
+        // Traditional
+
+        for (int i = 0; i < classes.length; i++) {
+            System.out.println(classes[i]);
+
+        }
+        System.out.println("-----------------------------------");
+        // for each
+
+        for (String word : classes) {
+            System.out.println(word);
+        }
+
+        // double example
+
+        System.out.println();
+
+        double[] prices = {10.4, 40.2, 410.2};
+
+        // for each
+
+        for (double each : prices) {
+            System.out.println("$ " + each);
+        }
+
+    }
+}
+
+
