a06daabfd9188818bb7d55ddccbdbde987a8fbb6
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-10-06 13:55:56 -0400

    MultiArrayExample updated

--- src/day32_arrays/MultiArrayExample.java
+++ src/day32_arrays/MultiArrayExample.java
@@ -10,14 +10,36 @@ public class MultiArrayExample {
         int [] a = {2022, 1990, 1987};
         int [] b = {03, 8, 29};
 
-        System.out.println(Arrays.toString(a)); // This is single
-        System.out.println(Arrays.toString(b)); // This is single
+        System.out.println("Single: " + Arrays.toString(a)); // This is single
+        System.out.println("Single: " + Arrays.toString(b)); // This is single
 
         System.out.println("---------------Multidimensional Array---------------------");
 
         int [] [] multi = {a, b};
 
-        System.out.println(Arrays.deepToString(multi)); // This is multidimensional Array
+        System.out.println("2D: " + Arrays.deepToString(multi)); // This is multidimensional Array
+
+
+        System.out.println("-------------------Multi 2 example-----------------");
+
+        int [] [] multiTwo = {
+                {10, 20, 30},  //  index 0 Array
+                {40, 50, 60},  //  index 1 Array
+                {70, 80, 90}   //  index 2 Array
+        };
+
+        System.out.println(Arrays.deepToString(multiTwo));
+        System.out.println(Arrays.toString(multiTwo[0]));
+        System.out.println(Arrays.toString(multiTwo[1]));
+        System.out.println(Arrays.toString(multiTwo[2]));
+        System.out.println(multiTwo.length);
+        System.out.println(multiTwo[0].length);
+        System.out.println(multiTwo[1].length);
+        System.out.println(multiTwo[2].length);
+        System.out.println(multiTwo[0] [2]);
+        System.out.println(multiTwo[1] [1]);
+
+
 
 
 
