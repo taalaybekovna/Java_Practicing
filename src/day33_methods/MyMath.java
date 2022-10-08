ba23ca5ecab28b48dac8c038529f9afdcc129f3e
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-10-08 09:22:08 -0400

    MyMath

---
+++ src/day33_methods/MyMath.java
@@ -0,0 +1,25 @@
+package day33_methods;
+
+public class MyMath {
+
+    public static void Add(double One, double Two){
+        System.out.println(One + " + " + Two + " = " + (One+Two));
+    }
+
+    public static void Plus(int Three, int Four){
+        System.out.println(Three + " + " + Four + " = " + (Three+Four));
+    }
+
+    public static void main(String[] args) {
+
+        Add(5.1, 6.1);
+        Add(100.9, 300.8);
+        Add(8.1, 8.7);
+        Plus(5,6);
+        Plus(1990, 1987);
+
+
+
+
+    }
+}
