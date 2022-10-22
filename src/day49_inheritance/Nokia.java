b8882ff32fa07ea1d44d8a4fd8c47467a1b67cc5
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-10-22 19:54:58 -0400

    Nokia

---
+++ src/day49_inheritance/Nokia.java
@@ -0,0 +1,24 @@
+package day49_inheritance;
+
+public class Nokia {
+
+    public String brand;
+    private int price;
+
+    public void sell(){
+        System.out.println(brand + " is trending now");
+    }
+
+    public void trend(){
+        System.out.println(price + " is fine for people");
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/day49_inheritance/UsingNokia.java
@@ -0,0 +1,24 @@
+package day49_inheritance;
+
+public class UsingNokia {
+
+    public static void main(String[] args) {
+
+        Nokia nokia = new Nokia();
+        nokia.brand = "A One";
+        System.out.println(nokia.brand);
+        nokia.sell();
+
+        Nokia nokia1 = new Nokia();
+        nokia1.setPrice(800);
+        nokia1.trend();
+
+
+    }
+
+
+
+
+
+
+}
